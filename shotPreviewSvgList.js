772093c88443cfde79c2812c1cf51e3cd6cd198f
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-10-22 11:52:18 -0400

    index-update-15 / script-update-30
    initial addition of shotPreviewSvgList.js script / code clean up

---
+++ shotPreviewSvgList.js
@@ -0,0 +1,253 @@
+/* ------------------------ Shot Preview SVG Objects ------------------------ */
+
+var backhand16 = {
+  width: "5.3911mm",
+  height: "49.896mm",
+  viewBox: "0 0 5.3910508 49.895966",
+  transform: "translate(-10.156 -5.2008)",
+  d: "m10.279 55.048c11.562-29.204 0-49.782 0-49.782",
+  strokeDashOffset: "51.20357894897461"
+};
+
+var forehand16 = {
+  width: "5.3911mm",
+  height: "49.896mm",
+  viewBox: "0 0 5.3910508 49.895966",
+  transform: "translate(-10.156 -5.2008)",
+  d: "m15.424 55.048c-11.562-29.204 0-49.782 0-49.782",
+  strokeDashOffset: "51.20357894897461"
+};
+
+
+var backhand17 = {
+  width: "5.3869mm",
+  height: "52.542mm",
+  viewBox: "0 0 5.3868952 52.54184",
+  transform: "translate(-20.741 -8.4136)",
+  d: "m20.865 60.909c11.55-30.759 0-52.433 0-52.433",
+  strokeDashOffset: "53.783485412597656"
+};
+
+var forehand17 = {
+  width: "5.3869mm",
+  height: "52.542mm",
+  viewBox: "0 0 5.3868952 52.54184",
+  transform: "translate(-20.741 -8.4136)",
+  d: "m26.004 60.909c-11.55-30.759 0-52.433 0-52.433",
+  strokeDashOffset: "53.783485412597656"
+};
+
+
+var backhand18 = {
+  width: "5.3828mm",
+  height: "55.188mm",
+  viewBox: "0 0 5.3828359 55.187614",
+  transform: "translate(-6.38 -4.8229)",
+  d: "m6.5046 59.966c11.539-32.314 0-55.083 0-55.083",
+  strokeDashOffset: "56.369022369384766"
+};
+
+var forehand18 = {
+  width: "5.3828mm",
+  height: "55.188mm",
+  viewBox: "0 0 5.3828359 55.187614",
+  transform: "translate(-6.38 -4.8229)",
+  d: "m11.638 59.966c-11.539-32.314 0-55.083 0-55.083",
+  strokeDashOffset: "56.369022369384766"
+};
+
+
+var backhand19 = {
+  width: "5.3787mm",
+  height: "57.833mm",
+  viewBox: "0 0 5.3787189 57.833443",
+  transform: "translate(-12.43 -9.5476)",
+  d: "m12.555 67.338c11.527-33.868 0-57.733 0-57.733",
+  strokeDashOffset: "58.95989227294922"
+};
+
+var forehand19 = {
+  width: "5.3787mm",
+  height: "57.833mm",
+  viewBox: "0 0 5.3787189 57.833443",
+  transform: "translate(-12.43 -9.5476)",
+  d: "m17.683 67.338c-11.527-33.868 0-57.733 0-57.733",
+  strokeDashOffset: "58.95989227294922"
+};
+
+
+var backhand20 = {
+  width: "5.3746mm",
+  height: "60.479mm",
+  viewBox: "0 0 5.3746138 60.479271",
+  transform: "translate(-13.188 -9.7366)",
+  d: "m13.313 70.175c11.516-35.423 0-60.383 0-60.383",
+  strokeDashOffset: "61.5558967590332"
+};
+
+var forehand20 = {
+  width: "5.3746mm",
+  height: "60.479mm",
+  viewBox: "0 0 5.3746138 60.479271",
+  transform: "translate(-13.188 -9.7366)",
+  d: "m18.437 70.175c-11.516-35.423 0-60.383 0-60.383",
+  strokeDashOffset: "61.5558967590332"
+};
+
+
+var backhand21 = {
+  width: "5.3705mm",
+  height: "63.125mm",
+  viewBox: "0 0 5.3704953 63.125122",
+  transform: "translate(-10.166 -6.9017)",
+  d: "m10.292 69.988c11.506-36.977 0-63.032 0-63.032",
+  strokeDashOffset: "64.15535736083984"
+};
+
+var forehand21 = {
+  width: "5.3705mm",
+  height: "63.125mm",
+  viewBox: "0 0 5.3704953 63.125122",
+  transform: "translate(-10.166 -6.9017)",
+  d: "m15.41 69.988c-11.506-36.977 0-63.032 0-63.032",
+  strokeDashOffset: "64.15535736083984"
+};
+
+
+var backhand22 = {
+  width: "5.3664mm",
+  height: "65.771mm",
+  viewBox: "0 0 5.3664412 65.77095",
+  transform: "translate(-8.6561 -4.0669)",
+  d: "m8.7828 69.8c11.495-38.531 0-65.682 0-65.682",
+  strokeDashOffset: "66.75943756103516"
+};
+
+var forehand22 = {
+  width: "5.3664mm",
+  height: "65.771mm",
+  viewBox: "0 0 5.3664412 65.77095",
+  transform: "translate(-8.6561 -4.0669)",
+  d: "m13.896 69.8c-11.495-38.531 0-65.682 0-65.682",
+  strokeDashOffset: "66.75943756103516"
+};
+
+
+var backhand23 = {
+  width: "5.3629mm",
+  height: "68.417mm",
+  viewBox: "0 0 5.3628645 68.416924",
+  transform: "translate(-7.9019 -8.7915)",
+  d: "m8.0293 77.172c11.485-40.085 0-68.331 0-68.331",
+  strokeDashOffset: "69.36614990234375"
+};
+
+var forehand23 = {
+  width: "5.3629mm",
+  height: "68.417mm",
+  viewBox: "0 0 5.3628645 68.416924",
+  transform: "translate(-7.9019 -8.7915)",
+  d: "m13.137 77.172c-11.485-40.085 0-68.331 0-68.331",
+  strokeDashOffset: "69.36614990234375"
+};
+
+
+var backhand24 = {
+  width: "5.3589mm",
+  height: "71.063mm",
+  viewBox: "0 0 5.3588648 71.062744",
+  transform: "translate(-10.172 -5.2008)",
+  d: "m10.3 76.228c11.475-41.639 0-70.979 0-70.979",
+  strokeDashOffset: "71.97493743896484"
+};
+
+var forehand24 = {
+  width: "5.3589mm",
+  height: "71.063mm",
+  viewBox: "0 0 5.3588648 71.062744",
+  transform: "translate(-10.172 -5.2008)",
+  d: "m15.407 76.228c-11.475-41.639 0-70.979 0-70.979",
+  strokeDashOffset: "71.97493743896484"
+};
+
+
+var backhand25 = {
+  width: "5.3549mm",
+  height: "73.709mm",
+  viewBox: "0 0 5.3549094 73.708565",
+  transform: "translate(-26.805 -13.705)",
+  d: "m26.933 87.38c11.465-43.192 0-73.628 0-73.628",
+  strokeDashOffset: "74.58731842041016"
+};
+
+var forehand25 = {
+  width: "5.3549mm",
+  height: "73.709mm",
+  viewBox: "0 0 5.3549094 73.708565",
+  transform: "translate(-26.805 -13.705)",
+  d: "m32.032 87.38c-11.465-43.192 0-73.628 0-73.628",
+  strokeDashOffset: "74.58731842041016"
+};
+
+
+var backhand26 = {
+  width: "5.3506mm",
+  height: "76.354mm",
+  viewBox: "0 0 5.3505793 76.354256",
+  transform: "translate(-13.2 -9.3586)",
+  d: "m13.328 85.68c11.455-44.746 0-76.276 0-76.276",
+  strokeDashOffset: "77.20118713378906"
+};
+
+var forehand26 = {
+  width: "5.3506mm",
+  height: "76.354mm",
+  viewBox: "0 0 5.3505793 76.354256",
+  transform: "translate(-13.2 -9.3586)",
+  d: "m18.422 85.68c-11.455-44.746 0-76.276 0-76.276",
+  strokeDashOffset: "77.20118713378906"
+};
+
+
+var backhand27 = {
+  width: "5.3467mm",
+  height: "79mm",
+  viewBox: "0 0 5.3466983 79.000092",
+  transform: "translate(-29.077 -14.083)",
+  d: "m29.205 93.052c11.446-46.3 0-78.925 0-78.925",
+  strokeDashOffset: "79.81852722167969"
+};
+
+var forehand27 = {
+  width: "5.3467mm",
+  height: "79mm",
+  viewBox: "0 0 5.3466983 79.000092",
+  transform: "translate(-29.077 -14.083)",
+  d: "m34.295 93.052c-11.446-46.3 0-78.925 0-78.925",
+  strokeDashOffset: "79.81852722167969"
+};
+
+
+var backhand28 = {
+  width: "5.3429mm",
+  height: "81.646mm",
+  viewBox: "0 0 5.3428669 81.645798",
+  transform: "translate(-32.102 -32.415)",
+  d: "m32.231 114.03c11.437-47.853 0-81.573 0-81.573",
+  strokeDashOffset: "82.43675231933594"
+};
+
+var forehand28 = {
+  width: "5.3429mm",
+  height: "81.646mm",
+  viewBox: "0 0 5.3428669 81.645798",
+  transform: "translate(-32.102 -32.415)",
+  d: "m37.321 114.03c-11.437-47.853 0-81.573 0-81.573",
+  strokeDashOffset: "82.43675231933594"
+};
+
+
+/* --------------------- Shot Preview SVG Object Arrays --------------------- */
+
+var shotPreviewArrayBackhand = [backhand16, backhand17, backhand18, backhand19, backhand20, backhand21, backhand22, backhand23, backhand24, backhand25, backhand26, backhand27, backhand28];
+var shotPreviewArrayForehand = [forehand16, forehand17, forehand18, forehand19, forehand20, forehand21, forehand22, forehand23, forehand24, forehand25, forehand26, forehand27, forehand28];
